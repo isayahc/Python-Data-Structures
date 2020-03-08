f998054e8dde2049fc05018442f6d5bb7c9ab325
Isayah Culbertson <isayahculbertson@gmail.com>
2020-03-07 19:15:18 -0500

    added another graph template

---
+++ GraphII.py
@@ -0,0 +1,66 @@
+from collections import deque
+graph = {'A': ['B', 'C'],
+'B': ['C', 'D'],
+'C': ['D'],
+'D': ['C'],
+'E' : ['F'],
+'F': ['C']
+}
+
+def find_path(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return path
+    if start not in graph:
+        return None
+    for node in graph[start]:
+        if node not in path:
+            newpath = find_path(graph, node, end, path)
+            if newpath : return newpath
+        return None
+
+def find_all_paths(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return [path]
+    if start not in graph:
+        return []
+    paths = []
+    for node in graph[start]:
+        if node not in path:
+            newpaths = find_all_paths(graph, node, end, path)
+            for newpaths in newpaths:
+                path.append(newpaths)
+    return paths
+
+# def find_shortest_path(graph, start, end, path=[]):
+#     path = path + [start]
+#     if start == end:
+#         return path 
+#     if start not in graph:
+#         return None
+#     shortest = None
+#     for node in graph[start]:
+#         if node not in path:
+#             newpath = find_shortest_path(graph, node, end, path)
+#             if newpath:
+#                 if not shortest or len(newpath) < len(shortest):
+#                     shortest = newpath
+#     return shortest
+
+def find_shortest_path(graph, start, end):
+    dist = {start: [start]}
+    q = deque(start)
+    while len(q):
+        at =  q.popleft()
+        for next in graph[at]:
+            if next in graph[at]:
+                if next not in dist:
+                    dist[next] = [dist[at], next]
+                    q.append(next)
+    return dist.get(end)
+
+
+
+print(find_path(graph, 'A', 'D'))
+print(find_shortest_path(graph,'A', 'D'))
