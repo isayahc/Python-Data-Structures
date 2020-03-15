cd9a672f64e23a9caec134ec49ec0753716afd38
unknown <isayahculbertson@gmail.com>
2020-03-15 19:10:42 -0400

    add

---
+++ Stack/Stack.py
@@ -0,0 +1,44 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+
+# class Stack:
+#      def __init__(self):
+#          self.items = []
+
+#      def isEmpty(self):
+#          return self.items == []
+
+#      def push(self, item):
+#          self.items.append(item)
+
+#      def pop(self):
+#          return self.items.pop()
+
+#      def peek(self):
+#          return self.items[len(self.items)-1]
+
+#      def size(self):
+#          return len(self.items)
+
+
+if __name__=='__main__':
+    s = Stack()
+    print(s.isEmpty())
+    s.push(1)
+    print(s.isEmpty())
